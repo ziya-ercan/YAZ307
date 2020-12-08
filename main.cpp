314b73abb1043937014f0971fbbe7e7108b33a36
Ziya Ercan <ziya.ercan@engr.istinye.edu.tr>
2020-12-08 09:44:04 +0300

    /I works but liking problems

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-//#include <mysqlx/xdevapi.h>
+#include <mysqlx/xdevapi.h>
 
 int main() 
 {
-  //  mysqlx::Session sess("localhost", 3306, "root", "PJuCiRfF9e");
+  //mysqlx::Session sess("localhost", 3306, "root", "PJuCiRfF9e");
   std::cout<<"Hello World\n";
+
   return 0; 
 }  
